d26ddc17ae28399a1de710eed182f9421ee3a2e5
kraulain <kraulain@gmail.com>
2024-08-14 14:50:41 +0100

    #0 | Team | cleanup bootstrapped files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Backend repository for the RB-clean project
 
+RB Clean is a simple laundry pickup service service
+
 ## How to setup
 
 - https://expressjs.com/en/starter/generator.html
--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET home page. */
+/* 
+GET home page. 
+*/
 router.get('/health', function(req, res, next) {
   res.status(200);
   res.json({ "message": 'Up',  "timestamp" : Date.now()});
 });
+
 
 module.exports = router;
