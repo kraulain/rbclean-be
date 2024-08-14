7af9294b367893ae3d272ffa565e780469baa7b1
kraulain <kraulain@gmail.com>
2024-08-14 14:08:07 +0100

    #0 | Team | add health check endpoint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rbclean-be
+# Backend repository for the RB-clean project
+
+## How to setup
+
+- https://expressjs.com/en/starter/generator.html
--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/health', function(req, res, next) {
+  res.status(200);
+  res.json({ "message": 'Up',  "timestamp" : Date.now()});
+});
+
 module.exports = router;
